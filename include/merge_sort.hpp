a67bb31ce5ca258364c7adfe704209d3f458cedd
Alexander Poluektov <alexander.poluektov@gmail.com>
2011-08-11 01:43:26 +0200

    Slight cleaning-up.

--- include/merge_sort.hpp
+++ include/merge_sort.hpp
@@ -16,42 +16,41 @@ namespace merge_sort_aux
 {
 
 inline
-void merge(std::vector<size_t>& v, size_t low, size_t mid, size_t high,
+void merge(std::vector<size_t>& v, size_t const low, size_t const mid, size_t const high,
            std::vector<size_t>& buf)
 {
-    for (size_t i = low; i <= mid; ++i)
+    for (size_t i = low; i <= high; ++i)
         buf[i] = v[i];
-    for (size_t i = mid+1; i <= high; ++i)
-        buf[i] = v[i];
-
-    size_t s1 = mid + 1;
-    size_t s2 = high + 1;
 
     size_t i1 = low;
+    size_t const s1 = mid + 1;
+
     size_t i2 = mid + 1;
-    size_t i = low;
+    size_t const s2 = high + 1;
+
+    size_t k = low;
 
     while (i1 != s1 && i2 != s2)
         if (buf[i1] < buf[i2])
-            v[i++] = buf[i1++];
+            v[k++] = buf[i1++];
         else
-            v[i++] = buf[i2++];
+            v[k++] = buf[i2++];
 
     while (i1 != s1)
-        v[i++] = buf[i1++];
+        v[k++] = buf[i1++];
 
     while (i2 != s2)
-        v[i++] = buf[i2++];
+        v[k++] = buf[i2++];
 }
 
 inline
-void merge_sort_impl(std::vector<size_t>& v, size_t low, size_t high,
+void merge_sort_impl(std::vector<size_t>& v, size_t const low, size_t const high,
                      std::vector<size_t>& buf)
 {
     if (low == high)
         return;
 
-    size_t mid = (low + high) / 2;
+    size_t const mid = (low + high) / 2;
     merge_sort_impl(v, low, mid, buf);
     merge_sort_impl(v, mid+1, high, buf);
     merge(v, low, mid, high, buf);
