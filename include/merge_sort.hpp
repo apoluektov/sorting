ed43bc1f38445b505f7a5f7be62137717c596666
Alexander Poluektov <alexander.poluektov@gmail.com>
2011-08-11 01:39:27 +0200

    Recursive Merge Sort: amount of additional memory reduced.

--- include/merge_sort.hpp
+++ include/merge_sort.hpp
@@ -17,44 +17,44 @@ namespace merge_sort_aux
 
 inline
 void merge(std::vector<size_t>& v, size_t low, size_t mid, size_t high,
-           std::vector<size_t>& v1, std::vector<size_t>& v2)
+           std::vector<size_t>& buf)
 {
     for (size_t i = low; i <= mid; ++i)
-        v1[i-low] = v[i];
+        buf[i] = v[i];
     for (size_t i = mid+1; i <= high; ++i)
-        v2[i-mid-1] = v[i];
+        buf[i] = v[i];
 
-    size_t s1 = mid - low + 1;
-    size_t s2 = high - mid;
+    size_t s1 = mid + 1;
+    size_t s2 = high + 1;
 
-    size_t i1 = 0;
-    size_t i2 = 0;
+    size_t i1 = low;
+    size_t i2 = mid + 1;
     size_t i = low;
 
     while (i1 != s1 && i2 != s2)
-        if (v1[i1] < v2[i2])
-            v[i++] = v1[i1++];
+        if (buf[i1] < buf[i2])
+            v[i++] = buf[i1++];
         else
-            v[i++] = v2[i2++];
+            v[i++] = buf[i2++];
 
     while (i1 != s1)
-        v[i++] = v1[i1++];
+        v[i++] = buf[i1++];
 
     while (i2 != s2)
-        v[i++] = v2[i2++];
+        v[i++] = buf[i2++];
 }
 
 inline
 void merge_sort_impl(std::vector<size_t>& v, size_t low, size_t high,
-                     std::vector<size_t>& v1, std::vector<size_t>& v2)
+                     std::vector<size_t>& buf)
 {
     if (low == high)
         return;
 
     size_t mid = (low + high) / 2;
-    merge_sort_impl(v, low, mid, v1, v2);
-    merge_sort_impl(v, mid+1, high, v1, v2);
-    merge(v, low, mid, high, v1, v2);
+    merge_sort_impl(v, low, mid, buf);
+    merge_sort_impl(v, mid+1, high, buf);
+    merge(v, low, mid, high, buf);
 }
 
 } // namespace merge_sort_aux
@@ -62,9 +62,8 @@ void merge_sort_impl(std::vector<size_t>& v, size_t low, size_t high,
 inline
 void merge_sort(std::vector<size_t>& v)
 {
-    std::vector<size_t> v1(v.size());
-    std::vector<size_t> v2(v.size());
-    merge_sort_aux::merge_sort_impl(v, 0, v.size()-1, v1, v2);
+    std::vector<size_t> buf(v.size());
+    merge_sort_aux::merge_sort_impl(v, 0, v.size()-1, buf);
 }
 
 #endif // MERGESORT_HPP_INCLUDED_
