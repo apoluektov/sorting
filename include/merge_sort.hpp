0d94fc945f0d89bf2543a2c491991493ff3d9d7b
Alexander Poluektov <alexander.poluektov@gmail.com>
2011-09-03 08:49:30 +0200

    Merge Sort: the interface separated from the implementation.

--- include/merge_sort.hpp
+++ include/merge_sort.hpp
@@ -7,11 +7,6 @@
 #ifndef MERGESORT_HPP_INCLUDED_
 #define MERGESORT_HPP_INCLUDED_
 
-#include <vector>
-#include <cstddef>
-#include <algorithm>
-#include <functional>
-
 
 // iterators versions
 
@@ -32,87 +27,6 @@ void merge_sort(Cont& v);
 
 // implementation
 
-namespace merge_sort_aux
-{
-
-template <class RanIt, class Cmp, class T>
-inline
-void merge(RanIt b, RanIt m, RanIt e, Cmp const& c, std::vector<T>& buf)
-{
-    std::copy(b, e, buf.begin());
-
-    RanIt i1 = buf.begin();
-    RanIt const e1 = buf.begin() + std::distance(b, m);
-
-    RanIt i2 = e1;
-    RanIt const e2 = buf.begin() + std::distance(b, e);
-
-    RanIt k = b;
-
-    while (i1 != e1 && i2 != e2)
-        if (c(*i1, *i2))
-            *k++ = *i1++;
-        else
-            *k++ = *i2++;
-
-    while (i1 != e1)
-        *k++ = *i1++;
-
-    while (i2 != e2)
-        *k++ = *i2++;
-}
-
-template <class RanIt, class Cmp, class T>
-inline
-void merge_sort_impl(RanIt b, RanIt e, Cmp const& c, std::vector<T>& buf)
-{
-    if (b == e || b + 1 == e)
-        return;
-
-    RanIt m = b + (std::distance(b, e) + 1) / 2;
-
-    merge_sort_impl(b, m, c, buf);
-    merge_sort_impl(m, e, c, buf);
-    merge(b, m, e, c, buf);
-}
-
-} // namespace merge_sort_aux
-
-// iterators versions
-
-template <class RanIt, class Cmp>
-inline
-void merge_sort(RanIt b, RanIt e, Cmp c)
-{
-    typedef typename std::iterator_traits<RanIt>::value_type T;
-    std::vector<T> buf(std::distance(b, e)); // could use scoped_array instead
-    merge_sort_aux::merge_sort_impl(b, e, c, buf);
-}
-
-template <class RanIt>
-inline
-void merge_sort(RanIt b, RanIt e)
-{
-    typedef typename std::iterator_traits<RanIt>::value_type T;
-    return merge_sort(b, e, std::less<T>());
-}
-
-// container versions
-
-template <class Cont, class Cmp>
-inline
-void merge_sort(Cont& v, Cmp c)
-{
-    return merge_sort(v.begin(), v.end(), c);
-}
-
-
-template <class Cont>
-inline
-void merge_sort(Cont& v)
-{
-    return merge_sort(v.begin(), v.end());
-}
-
+#include <merge_sort_impl.hpp>
 
 #endif // MERGESORT_HPP_INCLUDED_
---
+++ include/merge_sort_bottomup_impl.hpp
@@ -0,0 +1,89 @@
+// Copyright (c) 2011 Alexander Poluektov (alexander.poluektov@gmail.com)
+//
+// Use, modification and distribution are subject to the
+// Boost Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MERGESORTBOTTOMUPIMPL_HPP_INCLUDED_
+#define MERGESORTBOTTOMUPIMPL_HPP_INCLUDED_
+
+#include <vector>
+#include <cstddef>
+#include <algorithm>
+#include <functional>
+
+
+namespace merge_sort_bottomup_aux
+{
+
+template <class RanIt, class Cmp, class T>
+inline
+void merge(RanIt b, RanIt m, RanIt e, Cmp const& c, std::vector<T>& buf)
+{
+    std::copy(b, e, buf.begin());
+
+    RanIt i1 = buf.begin();
+    RanIt const e1 = buf.begin() + std::distance(b, m);
+
+    RanIt i2 = e1;
+    RanIt const e2 = buf.begin() + std::distance(b, e);
+
+    RanIt k = b;
+
+    while (i1 != e1 && i2 != e2)
+        if (c(*i1, *i2))
+            *k++ = *i1++;
+        else
+            *k++ = *i2++;
+
+    while (i1 != e1)
+        *k++ = *i1++;
+
+    while (i2 != e2)
+        *k++ = *i2++;
+}
+
+} // namespace merge_sort_bottomup_aux
+
+// iterators versions
+
+template <class RanIt, class Cmp>
+inline
+void merge_sort_bottomup(RanIt b, RanIt e, Cmp c)
+{
+    using merge_sort_bottomup_aux::merge;
+
+    typedef typename std::iterator_traits<RanIt>::value_type T;
+    std::vector<T> buf(std::distance(b, e)); // scoped_array?
+
+    for (int i = 1; i < e - b; i *= 2)
+        for (int j = 0; j < e - b - i; j += 2*i)
+            merge(b+j, b+j+i, std::min(b+j+2*i, e), c, buf);
+}
+
+template <class RanIt>
+inline
+void merge_sort_bottomup(RanIt b, RanIt e)
+{
+    typedef typename std::iterator_traits<RanIt>::value_type T;
+    return merge_sort_bottomup(b, e, std::less<T>());
+}
+
+// container versions
+
+template <class Cont, class Cmp>
+inline
+void merge_sort_bottomup(Cont& v, Cmp c)
+{
+    return merge_sort_bottomup(v.begin(), v.end(), c);
+}
+
+template <class Cont>
+inline
+void merge_sort_bottomup(Cont& v)
+{
+    return merge_sort_bottomup(v.begin(), v.end());
+}
+
+
+#endif // MERGESORTBOTTOMUPIMPL_HPP_INCLUDED_
